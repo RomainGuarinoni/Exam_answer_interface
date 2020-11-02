c2ba840ef4d3f9c5707ddbac4f60800352f8c0c2
RomainGuarinoni <romain.guar91@gmail.com>
2020-11-02 22:38:43 +0100

    modif probleme

--- toeic_localStorage.js
+++ toeic_localStorage.js
@@ -7,59 +7,62 @@ let b_button = document.getElementById("b");
 let c_button = document.getElementById("c");
 let d_button = document.getElementById("d");
 let already_begin=false;
-let question =1;
+let question=1;
+localStorage.setItem("question",question);
 let question_number;
 
 start_button.addEventListener('click',function(){
     if(document.getElementById("number").value!=""){
-        question_number=document.getElementById("number").value;
+        localStorage.setItem("question_number", document.getElementById("number").value);
         document.getElementById("begin").disabled=false;
         stop_button.disabled=false;
-    start_button.disabled=true;
-    document.getElementById("form").style.display="none";
-    document.getElementById("main").style.height="auto";
-    document.getElementById("section").style.display="block";
-    document.getElementById("question_number").innerHTML=question;
-    for(let i=0;i<question_number;i++){
-        let p = document.createElement('p');
-        p.classList.add("p_answer");
-        document.getElementById("result_box").appendChild(p);
-        let span = document.createElement('span');
-        span.classList.add('span_question');
-        let tiret = document.createElement('span');
-        tiret.classList.add("tiret");
-        let span2=document.createElement('span');
-        span2.classList.add('span_answer');
-        p.appendChild(span);
-        p.appendChild(tiret);
-        p.appendChild(span2);
-    }
-    already_begin=true;
+        start_button.disabled=true;
+        document.getElementById("form").style.display="none";
+        document.getElementById("main").style.height="auto";
+        document.getElementById("section").style.display="block";
+        document.getElementById("question_number").innerHTML=localStorage.getItem("question");
+        for(let i=0;i<localStorage.getItem("question_number");i++){
+            let p = document.createElement('p');
+            p.classList.add("p_answer");
+            document.getElementById("result_box").appendChild(p);
+            let span = document.createElement('span');
+            span.classList.add('span_question');
+            let tiret = document.createElement('span');
+            tiret.classList.add("tiret");
+            let span2=document.createElement('span');
+            span2.classList.add('span_answer');
+            p.appendChild(span);
+            p.appendChild(tiret);
+            p.appendChild(span2);
+        }
+        already_begin=true;
     } 
 });
 let span_question_array=document.getElementsByClassName("span_question");
 let span_answer_array = document.getElementsByClassName("span_answer");
 let tiret_array = document.getElementsByClassName("tiret");
 const next=()=>{
-    if (question>=question_number){
+    if (localStorage.getItem("question")>=localStorage.get("question_number")){
         document.getElementById("end").innerHTML="<p>Le test est terminé</p>";
         document.getElementById("end").style.display="block";
     }
     else {
         question++;
-        document.getElementById("question_number").innerHTML=question;
+        localStorage.setItem("question",question);
+        document.getElementById("question_number").innerHTML=localStorage.getItem("question");
     }
 }
 const previous=()=>{
     if(question>1){
         question--;
-        document.getElementById("question_number").innerHTML=question; 
+        localStorage.setItem("question",question);
+        document.getElementById("question_number").innerHTML=localStorage.getItem("question"); 
     }
 }
 const display_answer=(question, answer)=>{
-    span_question_array[question-1].innerHTML=question;
+    span_question_array[question-1].innerHTML=localStorage.getItem("question");
     tiret_array[question-1].innerHTML= " - ";
-    span_answer_array[question-1].innerHTML=answer;
+    span_answer_array[question-1].innerHTML=localStorage.getItem("answer");
 }
 
 next_button.addEventListener('click',function(event){
@@ -72,22 +75,26 @@ previous_button.addEventListener('click', function(event){
 });
 a_button.addEventListener('click',function(event){
     event.stopPropagation;
-    display_answer(question, "A");
+    localStorage.setItem("answer","A");
+    display_answer(localStorage.getItem("question"), localStorage.getItem("answer"));
     next();
 });
 b_button.addEventListener('click',function(event){
     event.stopPropagation;
-    display_answer(question, "B");
+    localStorage.setItem("answer","B");
+    display_answer(localStorage.getItem("question"), localStorage.getItem("answer"));
     next();
 });
 c_button.addEventListener('click',function(event){
     event.stopPropagation;
-    display_answer(question, "C");
+    localStorage.setItem("answer","C");
+    display_answer(localStorage.getItem("question"), localStorage.getItem("answer"));
     next();
 });
 d_button.addEventListener('click',function(event){
     event.stopPropagation;
-    display_answer(question, "D");
+    localStorage.setItem("answer","D");
+    display_answer(localStorage.getItem("question"), localStorage.getItem("answer"));;
     next();
 });
 
@@ -103,4 +110,5 @@ document.getElementById("stop").addEventListener('click',function(){
     start_button.disabled=false;
     document.getElementById("section").style.display="none";
     document.getElementById("form").style.display="block";
+    localStorage.clear();
 });
